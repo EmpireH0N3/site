a9f5c7f4ee90e0dde9e9a5469dfbf47db06e199a
Matteo C. <hello@micorksen.eu>
2024-11-15 22:24:35 +0100

    Update embassies.data.js

--- assets/js/embassies.data.js
+++ assets/js/embassies.data.js
@@ -1,23 +1,21 @@
-(function(url) {
-  const img = new Image();
-  img.src = url;
-  img.onload = () => {
-    console.log('%c', [
-      'font-size: 1px;',
-      'line-height: ' + this.height + 'px;',
-      'padding: ' + this.height * .5 + 'px ' + this.width * .5 + 'px;',
-      'background-size: ' + this.width + 'px ' + this.height + 'px;',
-      'background: url(' + url + ');'
-    ].join(' '));
-  };
-})('https://h0n3.eu/assets/img/logo.png');
+console.groupCollapsed('%cEmpire H0N3', 'color:#FFF;font-size:24px;background:#032A61');
+console.log('Ce site utilise des ressources mises gratuitement à disposition par les gestionnaires Infratique.');
+if (!window.location.hostname.endsWith('.h0n3.eu')) {
+  console.log('⚠️ Ce site n\'est probablement pas un service impérial officiel.\nSoyez prudents lors de la recherche d\'informations à l\'aide de ce site.');
+}
+
+console.groupEnd();
 
 window.$H0N3 = window.$H0N3 || {};
 $H0N3.embassies = {
   Chartres: {
+    ambassador: 'One Type II de France',
     long: 48.450001,
     lat: 1.5,
-    email: 'chartres@amb.h0n3.eu',
-    rep: 'One Type II de France',
   },
 };
+
+for (const embassy in $H0N3.embassies) {
+  $H0N3.embassies[embassy].ambassador = $H0N3.embassies[embassy].ambassador || 'N/C pour le moment';
+  $H0N3.embassies[embassy].email = `${embassy.replace(' ', '').toLowerCase()}@amb.h0n3.eu`;
+}
