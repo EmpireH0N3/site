f923916836dcc345c85c23df2fefd9fc9853c75a
Matteo C. <hello@micorksen.eu>
2024-11-15 21:56:37 +0100

    Create embassies.data.js

---
+++ assets/js/embassies.data.js
@@ -0,0 +1,24 @@
+window.$H0N3 = window.$H0N3 || {}
+
+(function(url) {
+  const img = new Image()
+  img.src = url
+  img.onload = () => {
+    console.log('%c', [
+      'font-size: 1px;',
+      'line-height: ' + this.height + 'px;',
+      'padding: ' + this.height * .5 + 'px ' + this.width * .5 + 'px;',
+      'background-size: ' + this.width + 'px ' + this.height + 'px;',
+      'background: url(' + url + ');'
+    ].join(' '))
+  }
+})('https://h0n3.eu/assets/img/logo.png')
+
+$H0N3.embassies = {
+  Chartres: {
+    long: 48.450001,
+    lat: 1.5,
+    email: 'chartres@amb.h0n3.eu',
+    rep: 'One Type II de France',
+  },
+}
